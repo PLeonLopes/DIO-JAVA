dbf0a8993b315f55eff026b1a5f607e7e54f3d85
Pedro Lopes <109923335+PLeonLopes@users.noreply.github.com>
2025-04-13 14:21:08 -0300

    Adição desafio BancoDigital

---
+++ BancoDigital/Cliente.java
@@ -0,0 +1,14 @@
+public class Cliente {
+
+	private String nome;
+
+	// GETTERS E SETTERS
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
